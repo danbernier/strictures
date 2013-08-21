889f12cefb9f3644b4097ae9655e5b5b2118ee74
Dan Bernier <danbernier@gmail.com>
2013-08-20 21:22:08 -0400

    Remove some over-zealous specs to lighten things up.

--- specs/strictures_spec.rb
+++ specs/strictures_spec.rb
@@ -5,11 +5,9 @@ describe Strictures do
     it 'can recognize same types' do
       Strictures.check('hello', Strictures.any(String)).must_be :valid?
       Strictures.check(42, Strictures.any(Fixnum)).must_be :valid?
-      Strictures.check(%w[a b c], Strictures.any(Array)).must_be :valid?
     end
 
     it 'can recognize different types' do
-      Strictures.check('hello', Strictures.any(Fixnum)).wont_be :valid?
       Strictures.check(42, Strictures.any(Array)).wont_be :valid?
       Strictures.check(%w[a b c], Strictures.any(String)).wont_be :valid?
     end
@@ -17,10 +15,6 @@ describe Strictures do
     it 'can report disappointments' do
       Strictures.check('hello', Strictures.any(Fixnum)).errors.must_equal [
         'Expected a Fixnum, got a String: "hello"'
-      ]
-
-      Strictures.check(42, Strictures.any(Array)).errors.must_equal [
-        'Expected a Array, got a Fixnum: 42'
       ]
 
       Strictures.check(%w[a b c], Strictures.any(String)).errors.must_equal [
